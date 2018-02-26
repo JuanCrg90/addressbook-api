31df083f7d51a9657332751ac87a73ed3d3b2efe
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2018-02-26 14:05:03 -0600

    Add update endpoint

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,5 @@
 class ContactsController < ApplicationController
+  before_action :find_contact, only: %I[show update]
   def index
     response = Contact.all
 
@@ -6,14 +7,29 @@ class ContactsController < ApplicationController
   end
 
   def create
-    response = Contact.create(create_contact_params)
+    response = Contact.create(contact_params)
 
     render json: response, status: :created
   end
 
+  def show
+    render json: @contact, status: :ok
+  end
+
+  def update
+    response = @contact.update(contact_params)
+
+    render :nothing, status: :ok
+  end
+
   private
 
-  def create_contact_params
+  def contact_params
     params.require(:data).permit(:name, :phone, :email, :address)
+  end
+
+  def find_contact
+    @contact = Contact.where(id: params[:id]).first
+    render :nothing, status: :not_found unless @contact
   end
 end
