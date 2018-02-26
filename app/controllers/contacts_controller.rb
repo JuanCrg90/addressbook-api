e9c0644a7ccb5256d1d2e1b3e991389913cb1cfa
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2018-02-26 13:10:04 -0600

    Add create contact endpoint

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,4 +4,16 @@ class ContactsController < ApplicationController
 
     render json: response, status: :ok
   end
+
+  def create
+    response = Contact.create(create_contact_params)
+
+    render json: response, status: :created
+  end
+
+  private
+
+  def create_contact_params
+    params.require(:data).permit(:name, :phone, :email, :address)
+  end
 end
